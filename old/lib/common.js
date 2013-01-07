16c14594f923f708f149d5c8cd21147e3808d9e3
Robert Ferris <baffles@gmail.com>
2013-01-06 23:40:44 -0500

    Remove old JS from prior incarnation.

--- old/lib/common.js
+++
@@ -1,4 +0,0 @@
-//document.write("<script type=\"text/javascript\" src=\"http://misc.baf.cc/accjs/lib/prototype-1.6.0.3.js\"></script>");
-//document.write("<script type=\"text/javascript\" src=\"http://misc.baf.cc/accjs/lib/json2.js\"></script>");
-document.write("<script type=\"text/javascript\" src=\"http://misc.baf.cc/accjs/lib/cookiejar.js\"></script>");
-
