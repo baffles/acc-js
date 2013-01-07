3ba21d802e4b40bad6ec5ea05e303d18a9734cca
Robert Ferris <baffles@gmail.com>
2013-01-06 21:24:11 -0500

    Initial commit, with old accjs quote plugin.

---
+++ old/lib/common.js
@@ -0,0 +1,4 @@
+//document.write("<script type=\"text/javascript\" src=\"http://misc.baf.cc/accjs/lib/prototype-1.6.0.3.js\"></script>");
+//document.write("<script type=\"text/javascript\" src=\"http://misc.baf.cc/accjs/lib/json2.js\"></script>");
+document.write("<script type=\"text/javascript\" src=\"http://misc.baf.cc/accjs/lib/cookiejar.js\"></script>");
+
